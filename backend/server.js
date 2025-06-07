036a7c9de60f7c5fe0d2e7e23e8c6c22af0a2450
Jtucke327 <jtucke327@gmail.com>
2025-06-06 23:15:22 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.disable('x-powered-by');
 //Will update once I have domain name
 app.use(cors({
-  origin: ['https://your-frontend-domain.com'],
+  origin: ['https://league-of-ordinary-gentlemen.netlify.app'],
 }));
 app.use(express.json());
 
